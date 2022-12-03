59d3e3f72608954aaed2d8fa4e21d60a6e972428
Canario0 <prenero4bc@gmail.com>
2022-12-03 22:14:44 +0100

    refactor: remove unused code

--- src/lib/shared/infrastructure/components/footer.svelte
+++
@@ -1,8 +0,0 @@
-<footer>Test</footer>
-
-<style>
-  footer {
-    font-size: 1.6rem;
-    padding: 2rem;
-  }
-</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import { goto, invalidate } from "$app/navigation";
   import { page } from "$app/stores";
   import { supabaseClient } from "$lib/db";
-  import Footer from "$lib/shared/infrastructure/components/footer.svelte";
   import Header from "$lib/shared/infrastructure/components/header.svelte";
   import { onMount } from "svelte";
 
@@ -10,8 +9,6 @@
     const {
       data: { subscription }
     } = supabaseClient.auth.onAuthStateChange((event, session) => {
-      debugger;
-      console.log({ event, session });
       invalidate("supabase:auth");
       if (session) {
         goto("/", { invalidateAll: true });
@@ -28,7 +25,6 @@
 <main>
   <slot />
 </main>
-<Footer />
 
 <style>
   :global(body) {
@@ -44,11 +40,10 @@
   :global(#root) {
     display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: auto 1fr 100px;
+    grid-template-rows: auto 1fr;
     padding: 4px 4px 2px 4px;
   }
   main {
-    font-size: 1.6rem;
     padding: 2rem;
   }
 </style>
