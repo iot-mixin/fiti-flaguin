c71443d298bb7322e73b45f63c36cd08850f8caa
Ismael Taboada Rodero <ismael.jtr@gmail.com>
2022-12-04 09:44:11 +0100

    revert: recover login actions in server file

---
+++ src/routes/(public)/login/+page.server.ts
@@ -0,0 +1,34 @@
+import { getSupabase } from "@supabase/auth-helpers-sveltekit";
+import { AuthApiError } from "@supabase/supabase-js";
+import { invalid, redirect, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+  loginWithGoogle: async (event) => {
+    const { request } = event;
+    const { supabaseClient } = await getSupabase(event);
+
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: new URL(request.url).origin
+      }
+    });
+
+    if (error) {
+      if (error instanceof AuthApiError && error.status === 400) {
+        return invalid(400, {
+          method: "loginWithGoogle" as const,
+          error: "Invalid credentials." as const,
+          values: {}
+        });
+      }
+      return invalid(500, {
+        method: "loginWithGoogle" as const,
+        error: "Server error. Try again later." as const,
+        values: {}
+      });
+    }
+
+    throw redirect(303, data.url);
+  }
+};
