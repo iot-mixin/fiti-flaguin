0e955d80a2adfdf08badf1d51f7182364468c1fa
Canario0 <prenero4bc@gmail.com>
2023-03-22 20:41:46 +0100

    ci: wrong version

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h4", async ({ page }) => {
   await page.goto("/");
-  expect(await page.textContent("h4")).toBe("Welcome to Fiti Flaguin");
+  expect(await page.textContent("h4")).toBe("LOG In To Your account");
 });
